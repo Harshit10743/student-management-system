275605f4eb4c6c95b710cc58617cdb9dd28c410c
Harshit10743 <harshit3875.hm@gmail.com>
2026-06-16 11:41:09 +0530

    Day 9 JWT Token Complete, new filter added JWT Filter

--- backend/lms/src/main/java/com/example/lms/config/SecurityConfig.java
+++ backend/lms/src/main/java/com/example/lms/config/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.example.lms.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception 
@@ -16,6 +21,9 @@ public class SecurityConfig {
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
+                ).addFilterBefore (
+                    jwtFilter,
+                    UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();
--- backend/lms/src/main/java/com/example/lms/service/JWTService.java
+++ backend/lms/src/main/java/com/example/lms/service/JWTService.java
@@ -1,22 +1,43 @@
 package com.example.lms.service;
 
-import org.springframework.stereotype.Service;
-import io.jsonwebtoken.Jwts;
 import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+
 @Service
 public class JWTService {
+
     @Value("${jwt.secret}")
     private String Key;
-    public String generateJwtToken (String username) {
-        String token =
-
-        Jwts.builder()
+    public String generateJwtToken(String username) {
+        String token = Jwts.builder()
                 .setSubject(username)
-                .setExpiration(new Date(System.currentTimeMillis()+ 1000*60*30))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
                 .signWith(Keys.hmacShaKeyFor(Key.getBytes()))
                 .compact();
         return token;
+    }
+    public boolean isValid(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(Key.getBytes()))
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(Key.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
     }
 }
